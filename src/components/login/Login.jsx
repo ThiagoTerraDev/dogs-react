0fe297ec3401d9b09388f5d6bd3f5e9b0c2dbb30
Thiago Terra <thiagoterradev@gmail.com>
2025-03-11 12:54:14 -0300

    feat: implement Login background

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,11 +1,43 @@
 import React, { useContext } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import styled from 'styled-components';
 import LoginForm from './LoginForm';
 import Register from './Register';
 import PasswordLost from './PasswordLost';
 import PasswordReset from './PasswordReset';
 import { UserContext } from '../../UserContext';
+import LoginImage from '../../assets/login.jpg';
 
+
+const LoginSection = styled.section`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  min-height: 100vh;
+  gap: 2rem;
+
+  &::before {
+    content: '';
+    display: block;
+    background: url(${LoginImage}) no-repeat center center;
+    background-size: cover;
+  }
+
+  @media (max-width: 40rem) {
+    grid-template-columns: 1fr;
+    &::before {
+      display: none;
+    }
+  }
+`;
+
+const LoginForms = styled.div`
+  max-width: 30rem;
+  padding: 1rem;
+
+  @media (max-width: 40rem) {
+    max-width: 100%;
+  }
+`;
 
 const Login = () => {
   const { login } = useContext(UserContext);
@@ -13,14 +45,16 @@ const Login = () => {
   if (login === true) return <Navigate to="/account" />;
 
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="register" element={<Register />} />
-        <Route path="passwordlost" element={<PasswordLost />} />
-        <Route path="passwordreset" element={<PasswordReset />} />
-      </Routes>
-    </div>
+    <LoginSection>
+      <LoginForms>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="register" element={<Register />} />
+          <Route path="passwordlost" element={<PasswordLost />} />
+          <Route path="passwordreset" element={<PasswordReset />} />
+        </Routes>
+      </LoginForms>
+    </LoginSection>
   );
 };
 
