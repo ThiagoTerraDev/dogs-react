1ced50fbb1e75c23d4f2d97938fdad4ec4e8e430
Thiago Terra <thiagoterradev@gmail.com>
2025-04-15 19:10:51 -0300

    feat: add FeedPhotosItem component

---
+++ src/components/feed/FeedPhotosItem.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { styled } from 'styled-components';
+import ViewIcon from '../../assets/visualizacao.svg?react';
+
+
+const PhotoItem = styled.li`
+  list-style: none;
+  display: grid;
+  border-radius: 0.2rem;
+  overflow: hidden;
+  cursor: pointer;
+
+  img {
+    grid-area: 1 / 1;
+  }
+
+  span {
+    grid-area: 1 / 1;
+    background: rgba(0, 0, 0, 0.3);
+    color: #fff;
+    font-size: 1rem;
+    text-align: center;
+    display: none;
+    align-items: center;
+    justify-content: center;
+  }
+
+  &:hover span {
+    display: flex;
+  }
+`;
+
+const ViewIconStyled = styled(ViewIcon)`
+  display: inline-block;
+  margin-right: 0.25rem;
+`;
+
+
+const FeedPhotosItem = ({photo}) => {
+  return (
+    <PhotoItem>
+      <img src={photo.src} alt={photo.title} />
+      <span>
+        <ViewIconStyled />
+        {photo.acessos}
+      </span>
+    </PhotoItem>
+  );
+};
+
+export default FeedPhotosItem;
