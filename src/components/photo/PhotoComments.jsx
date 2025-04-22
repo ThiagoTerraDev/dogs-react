ee697b8c2d137453b1559efe451fdae1625f4efc
Thiago Terra <thiagoterradev@gmail.com>
2025-04-22 13:01:44 -0300

    feat: implement PhotoComments features

--- src/components/photo/PhotoComments.jsx
+++ src/components/photo/PhotoComments.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import { UserContext } from '../../UserContext';
+import PhotoCommentsForm from './PhotoCommentsForm';
+import { styled } from 'styled-components';
 
 
-const PhotoComments = () => {
+const PhotoCommentsStyled = styled.ul`
+  overflow-y: auto;
+  word-break: break-word;
+  padding: 0 2rem;
+
+  li {
+    margin-bottom: 0.5rem;
+    line-height: 1.2;
+  }
+`;
+
+
+const PhotoComments = (props) => {
+  const { login } = useContext(UserContext);
+  const [comments, setComments] = useState(() => props.comments);
+  const commentsSection = useRef(null);
+
+  useEffect(() => {
+    commentsSection.current.scrollTop = commentsSection.current.scrollHeight;
+  },[comments]);
+
   return (
-    <div>
-      ...
-    </div>
+    <>
+      <PhotoCommentsStyled ref={commentsSection}>
+        {comments.map(comment => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}: </b>
+            <span>{comment.comment_content}</span>
+          </li>
+          ))}
+      </PhotoCommentsStyled>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments} /> }
+    </>
   );
 };
 
